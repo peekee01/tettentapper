8f31d22cd05a27b04289cbcd3b4c66bb31fd9e90
Pieter Kuijsten <peekee@gmail.com>
2016-12-19 20:07:12 +0100

    test commit

--- tettentapper/ViewController.swift
+++ tettentapper/ViewController.swift
@@ -9,15 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Variables
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Outlets
+    @IBOutlet weak var tettentekstImg: UIImageView!
+    @IBOutlet weak var hoeveeltapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tellerLbl: UILabel!
+    
+    @IBAction func ontettentapBtn(sender: UIButton!){
+        currentTaps = currentTaps + 1
+        updatetapslbl()
+        
+        if gameover() {
+            restartgame()
+        }
+        
     }
+    
+    
+    @IBAction func onplayBtnpressed(sender: UIButton!) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if hoeveeltapsTxt.text != nil && hoeveeltapsTxt.text != "" {
+            
+            tettentekstImg.hidden = true
+            playBtn.hidden = true
+            hoeveeltapsTxt.hidden = true
+            tapBtn.hidden = false
+            tellerLbl.hidden = false
+            
+            maxTaps = Int(hoeveeltapsTxt.text!)!
+            currentTaps = 0
+            
+            updatetapslbl()
+        }
+    }
+        
+    func restartgame() {
+        maxTaps = 0
+        currentTaps = 0
+        hoeveeltapsTxt.text = nil
+        
+        tettentekstImg.hidden = false
+        playBtn.hidden = false
+        hoeveeltapsTxt.hidden = false
+        tapBtn.hidden = true
+        tellerLbl.hidden = true
+    }
+    
+    func gameover() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updatetapslbl() {
+        tellerLbl.text = "\(currentTaps) tetten getapt!"
     }
 
 
